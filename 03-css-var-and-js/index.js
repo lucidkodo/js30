1ff219bf2147b410fc82cad95ecc048d1a4ab58c
angie <lucidkodo@gmail.com>
2018-01-30 04:00:26 +0800

    Complete 03

--- 03-css-var-and-js/index.js
+++ 03-css-var-and-js/index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+const inputs = document.querySelectorAll('.controls input')
+
+function handleUpdate () {
+  const suffix = this.dataset.sizing || ''
+  // console.log(this.name) // js have access to DOM when there's a name attr in it
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+}
+
+inputs.forEach(input => {
+  input.addEventListener('change', handleUpdate)
+  input.addEventListener('mousemove', handleUpdate)
+})
